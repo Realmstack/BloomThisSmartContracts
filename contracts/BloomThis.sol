9322a6ff0cb6a6b7f16623f1e9e2a10c0a6593f7
arunzn <arun@zoyride.com>
2022-12-23 16:45:08 +0530

    Update BloomThis.sol

--- contracts/BloomThis.sol
+++ contracts/BloomThis.sol
@@ -8,12 +8,14 @@ contract BloomThis is ERC721 {
     mapping(address=> uint8) public _admins;
     address[] public _adminList;
 
-    uint256 public tokenCounter;
+    uint256 public _tokenCounter;
     uint256 public _maxTokens;
     mapping(uint256 => string) public baseUris;
+    mapping(address => uint256[]) private _userTokens;
+    mapping(uint256=> uint256) private _tokenIndex;
 
-    constructor () public ERC721 ("Bloom This", "BLUM"){
-        tokenCounter = 0;
+    constructor () ERC721 ("Bloom This", "BLUM"){
+        _tokenCounter = 0;
         _admins[msg.sender] = 1;
         _adminList.push(msg.sender);
         _maxTokens = 27;
@@ -41,18 +43,39 @@ contract BloomThis is ERC721 {
         }
     }
 
-    function createCollectible(address receiver, string memory tokenURI) public returns (uint256) {
-        require(_maxTokens > tokenCounter, "Tokens exhausted");
-        uint256 newItemId = tokenCounter + 1;
+    function createCollectible(address receiver, string memory tokenURL) public returns (uint256) {
+        require(_maxTokens > _tokenCounter, "Tokens exhausted");
+        uint256 newItemId = _tokenCounter + 1;
         _safeMint(receiver, newItemId);
-        baseUris[newItemId] = tokenURI;
-        tokenCounter = tokenCounter + 1;
+        // _setTokenURI(newItemId, tokenURI);
+        baseUris[newItemId] = tokenURL;
+        _tokenCounter = _tokenCounter + 1;
+        _userTokens[receiver].push(newItemId);
+        _tokenIndex[newItemId] = _userTokens[receiver].length - 1;
         return newItemId;
     }
 
 
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        require(tokenId > 0 && tokenId <= tokenCounter, "Invalid token id");
+        require(tokenId > 0 && tokenId <= _tokenCounter, "Invalid token id");
         return baseUris[tokenId];
+    }
+
+
+    function transferFrom(address _from, address _to, uint256 _tokenId) override public {
+        super.transferFrom(_from, _to, _tokenId);
+        
+        //delete token entry from previous user 
+        _userTokens[_from][_tokenIndex[_tokenId]] = _userTokens[_from][_userTokens[_from].length -1];
+        _tokenIndex[_userTokens[_from][_tokenIndex[_tokenId]]] = _tokenIndex[_tokenId];
+        _userTokens[_from].pop();
+
+        //add token to index of new user
+        _userTokens[_to].push(_tokenId);
+        _tokenIndex[_tokenId] = _userTokens[_to].length - 1;
+    }
+
+    function userTokens(address user) public view returns(uint256[] memory) {
+        return _userTokens[user];
     }
 }
